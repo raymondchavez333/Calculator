0e8857b04702ecc51524ef3710326fa050af7e1c
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-28 23:01:58 +0800

    Add a function caled operate

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ console.log(add(1,2));
 console.log(subtract(1,2));
 console.log(multiply(1,2));
 console.log(divide(1, 2));
+
+function operate (operator, num1, num2){
+
+    if(operator === '+'){
+        return add(num1, num2);
+    }
+    if(operator === '-'){
+        return subtract(num1, num2);
+    }
+    if(operator === '*'){
+        return multiply(num1, num2);
+    }
+    if(operator === '+'){
+        return divide(num1, num2);
+    }
+
+    return;
+}
+
+console.log(operate('-', 1, 2));

c105ee24e9ef6ce3a8b2a0fc2a313d7902f63a25
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-28 23:03:54 +0800

    fix the operator conditon for division within the operate function

--- script.js
+++ script.js
@@ -18,11 +18,6 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(multiply(1,2));
-console.log(divide(1, 2));
-
 function operate (operator, num1, num2){
 
     if(operator === '+'){
@@ -34,11 +29,11 @@ function operate (operator, num1, num2){
     if(operator === '*'){
         return multiply(num1, num2);
     }
-    if(operator === '+'){
+    if(operator === '/'){
         return divide(num1, num2);
     }
 
     return;
 }
 
-console.log(operate('-', 1, 2));
+console.log(operate('', 1, 2));
